af4ede11cdab0fcabd82481f3414de8391b72c14
rahina ummer <raahinaashik@gmail.com>
2026-03-04 23:05:52 +0530

    fix google oauth redirect

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://rahina.online/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
